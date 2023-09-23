d9213dc0f552a05ff7e3daf4c48dab3a57c5c769
28018 <2801819967@qq.com>
2023-09-23 18:16:04 +0800

    删分号;

--- src/main/kotlin/cn/jjaw/easyevent/EventManager.kt
+++ src/main/kotlin/cn/jjaw/easyevent/EventManager.kt
@@ -3,8 +3,8 @@ package cn.jjaw.easyevent
  * 事件管理器
  */
 class EventManager {
-    private val eventSuperClass = Event::class.java;
-    private val eventTypeGroupMap:EventTypeGroupMap = EventTypeGroupMap();
+    private val eventSuperClass = Event::class.java
+    private val eventTypeGroupMap:EventTypeGroupMap = EventTypeGroupMap()
 
     /**
      * 注册事件监听器
@@ -14,8 +14,8 @@ class EventManager {
      * @return 事件监听器对象
      */
     fun <T: Event> regEventListen(type:Class<T>, execute:(T)->Unit,priority: Priority = Priority.Medium):EventListen<T>{
-        val group = eventTypeGroupMap.computeIfNew(type);
-        return group.addEventListen(priority,execute);
+        val group = eventTypeGroupMap.computeIfNew(type)
+        return group.addEventListen(priority,execute)
     }
     /**
      * 注册事件监听器
@@ -24,8 +24,8 @@ class EventManager {
      * @return 事件监听器对象
      */
     fun <T: Event> regEventListen(type:Class<T>, ex:(T)->Unit):EventListen<T>{
-        val group = eventTypeGroupMap.computeIfNew(type);
-        return group.addEventListen(Priority.Medium,ex);
+        val group = eventTypeGroupMap.computeIfNew(type)
+        return group.addEventListen(Priority.Medium,ex)
     }
 
     /**
@@ -36,13 +36,13 @@ class EventManager {
         var exClass:Class<*>? = event.javaClass
         cs@while (exClass!=null) {
             if (event.stopPropagation || event.stopImmediatePropagation) {
-                break@cs;
+                break@cs
             }
             eventTypeGroupMap.get(exClass as Class<Event>)?.execute(event);
             if (exClass == eventSuperClass) {
-                break@cs;
+                break@cs
             }
-            exClass = exClass.superclass;
+            exClass = exClass.superclass
         }
     }
 }
@@ -55,10 +55,10 @@ class EventManager {
 class EventTypeGroupMap{
     private val map:MutableMap<Class<*>,EventTypeGroup<*>> = mutableMapOf();
     internal fun <T: Event> put(type:Class<T>, execute:EventTypeGroup<T>):EventTypeGroup<T>?{
-        return map.put(type,execute) as EventTypeGroup<T>?;
+        return map.put(type,execute) as EventTypeGroup<T>?
     }
     internal fun <T: Event> get(type:Class<T>):EventTypeGroup<T>? {
-        return map.get(type) as EventTypeGroup<T>?;
+        return map.get(type) as EventTypeGroup<T>?
     }
     internal fun <T: Event> computeIfNew(type:Class<T>):EventTypeGroup<T>{
         return map.computeIfAbsent(type){ EventTypeGroup<T>() } as EventTypeGroup<T>;
@@ -96,21 +96,21 @@ val PriorityOrder = arrayOf(
     Priority.Medium,
     Priority.Low,
     Priority.ExtremelyLow
-);
+)
 
 class EventTypeGroup<T: Event>{
-    private val priorityGroupMap:MutableMap<Priority,PriorityGroup<T>> = mutableMapOf();
+    private val priorityGroupMap:MutableMap<Priority,PriorityGroup<T>> = mutableMapOf()
 
     internal fun addEventListen(priority:Priority,ex:(event:T)->Unit):EventListen<T>{
-        return priorityGroupMap.computeIfAbsent(priority){PriorityGroup()}.addEventListen(ex);
+        return priorityGroupMap.computeIfAbsent(priority){PriorityGroup()}.addEventListen(ex)
     }
 
     internal fun execute(event:T){
         all@for (the in PriorityOrder){
             if (event.stopImmediatePropagation){
-                break@all;
+                break@all
             }
-            priorityGroupMap[the]?.execute(event);
+            priorityGroupMap[the]?.execute(event)
         }
     }
 }
@@ -119,26 +119,26 @@ class EventTypeGroup<T: Event>{
  * 事件类型组
  */
 class PriorityGroup<T: Event>{
-    private val eventListens:MutableList<EventListen<T>> = mutableListOf();
+    private val eventListens:MutableList<EventListen<T>> = mutableListOf()
 
     internal fun addEventListen(ex:(event:T)->Unit):EventListen<T>{
-        val eventListen = EventListen(this,ex);
-        this.eventListens.add(eventListen);
-        return eventListen;
+        val eventListen = EventListen(this,ex)
+        this.eventListens.add(eventListen)
+        return eventListen
     }
 
     internal fun removeEventListen(eventListen: EventListen<T>){
-        this.eventListens.remove(eventListen);
+        this.eventListens.remove(eventListen)
     }
     internal fun execute(event:T){
         all@for(exe in eventListens){
             if(event.stopImmediatePropagation){
-                break@all;
+                break@all
             }
             try {
-                exe.execute(event);
+                exe.execute(event)
             }catch (a:Throwable){
-                a.printStackTrace();
+                a.printStackTrace()
             }
         }
     }
@@ -153,13 +153,13 @@ class EventListen<T: Event>(private val group:PriorityGroup<T>, private val ex:(
      * 关闭这个监听器，不再监听事件
      */
     fun close(){
-        this.group.removeEventListen(this);
+        this.group.removeEventListen(this)
     }
 
     /**
      * 执行这个事件
      */
     fun execute(event:T){
-        this.ex(event);
+        this.ex(event)
     }
 }
